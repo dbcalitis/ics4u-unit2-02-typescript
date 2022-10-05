5b96f06a5b61b539dc31e211b9e3d4463a21e4fd
dbcalitis <daria.bernice.calitis@mths.ca>
2022-10-05 23:29:43 +0000

    init

---
+++ \
@@ -0,0 +1,24 @@
+/**
+ * MrCoxallStack Class
+ * By:      Daria Bernice Calitis
+ * Version: 1.0
+ * Since:   2022-10-03
+ */
+
+class MrCoxallStack {
+  private stack: number[] = []
+
+  getStack(): number[] {
+    return this.stack
+  }
+
+  pushItem(pushedNumber: number): void {
+    this.stack.push(pushedNumber)
+  }
+
+  popItem(): number | undefined {
+    return this.stack.pop()
+  }
+}
+
+export = MrCoxallStack
---
+++ src/index.ts
@@ -0,0 +1,33 @@
+/**
+ * This is the main code that runs
+ * with the MrCoxallStack.
+ * By:      Daria Bernice Calitis
+ * Version: 1.0
+ * Since:   2022-10-03
+ */
+
+import MrCoxallStack from './MrCoxallStack'
+import promptSync from 'prompt-sync'
+
+// use MrCoxallStack and promptSync classes
+const prompt = promptSync()
+const myStack = new MrCoxallStack()
+
+// Input & Process
+const userNum1 = Number(prompt('Enter a number: '))
+myStack.pushItem(userNum1)
+
+const userNum2 = Number(prompt('Enter a second number: '))
+myStack.pushItem(userNum2)
+
+const userNum3 = Number(prompt('Enter a third number: '))
+myStack.pushItem(userNum3)
+
+// Output
+console.log(myStack.getStack())
+
+console.log(`\nPopped number: ${myStack.popItem()}`)
+
+console.log(myStack.getStack())
+
+console.log('\nDone.')
