6dff6cf09819d3a45061bddd7be5d6c8336104cd
dbcalitis <daria.bernice.calitis@mths.ca>
2022-10-10 23:09:13 +0000

    updated MrCoxallStack.ts

--- src/MrCoxallStack.ts
+++ src/MrCoxallStack.ts
@@ -8,14 +8,17 @@
 class MrCoxallStack {
   private readonly stack: number[] = []
 
+  # getter
   getStack(): number[] {
     return this.stack
   }
 
+  # pushItem - Adds a new item in stack array.
   pushItem(pushedNumber: number): void {
     this.stack.push(pushedNumber)
   }
 
+  # popItem - Returns and removes the last item from the stack array.
   popItem(): number | undefined {
     return this.stack.pop()
   }
